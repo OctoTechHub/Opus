9638668d88f26453c9ab6a1d1d1695311b98f5a4
Krish Soni <sonikrish994@gmail.com>
2024-06-13 15:26:21 +0530

    payment added

--- Opus-Stellar/src/pages/BuyLand.tsx
+++ Opus-Stellar/src/pages/BuyLand.tsx
@@ -1,18 +1,24 @@
-// BuyLand component
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import OpusMap from "./components/OpusMap";
+import PaymentComponent from "./components/PaymentComponent";
 
 const BuyLand = () => {
   const location = useLocation();
   const [publicKey, setPublicKey] = useState('');
+  const [secretKey, setSecretKey] = useState('');
 
   useEffect(() => {
+    console.log('useEffect hook executed');
     if (location.state) {
-      const { publicKey } = location.state;
+      const { publicKey, secretKey } = location.state;
       setPublicKey(publicKey);
+      setSecretKey(secretKey);
     }
   }, [location]);
+
+  console.log('PublicKey:', publicKey);
+  console.log('SecretKey:', secretKey);
 
   return (
     <div className="bg-gradient-to-br from-blue-400 to-purple-600 min-h-screen flex flex-col justify-center items-center">
@@ -21,9 +27,14 @@ const BuyLand = () => {
         <p className="text-lg mb-6 text-center text-gray-700">Explore available land:</p>
         <nav className="text-center text-gray-600 mb-4">
           Public Key: {publicKey}
+          <br />
+          Secret Key: {secretKey}
         </nav>
         <div className="w-full max-w-screen-lg">
-          <OpusMap publicKey={publicKey} secretKey={location.state.secretKey} />
+          <OpusMap publicKey={publicKey} secretKey={secretKey} />
+        </div>
+        <div className="mt-8">
+          <PaymentComponent publicKey={publicKey} secretKey={secretKey} />
         </div>
       </div>
     </div>

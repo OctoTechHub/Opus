b111abb64bbeedb4af1875b18eb41d0cb5eb8d8e
Krish Soni <sonikrish994@gmail.com>
2024-06-14 22:07:57 +0530

    commited to you <3

--- Opus-Stellar/src/pages/BuyLand.tsx
+++ Opus-Stellar/src/pages/BuyLand.tsx
@@ -11,24 +11,7 @@ const BuyLand = () => {
   const [privateKey, setPrivateKey] = useState('');
   const [copySuccess, setCopySuccess] = useState(''); 
 
-  useEffect(() => {
-    console.log('useEffect hook executed');
-    if (location.state) {
-      const { publicKey, privateKey } = location.state;
-      setPublicKey(publicKey);
-      setPrivateKey(privateKey);
-    } else {
-      const storedPublicKey = localStorage.getItem('publicKey');
-      const storedPrivateKey = localStorage.getItem('privateKey');
-      if (storedPublicKey && storedPrivateKey) {
-        setPublicKey(storedPublicKey);
-        setPrivateKey(storedPrivateKey);
-      } else {
-        console.warn('Public key or private key not found in local storage.');
-        navigate('/'); // Redirect to home page if keys are not found
-      }
-    }
-  }, [location, navigate]);
+
 
   const handleCopyToClipboard = async (key: string) => {
     try {
@@ -43,20 +26,13 @@ const BuyLand = () => {
   return (
     <>
       <Navbar />
-      <div className="bg-gradient-to-br from-blue-400 to-purple-600 min-h-screen flex flex-col justify-center items-center">
+      <div className="bg-gradient-to-br from-gray-800 to-black min-h-screen flex flex-col justify-center items-center">
         <div className="bg-white bg-opacity-80 p-6 rounded-lg shadow-lg">
           <h2 className="text-3xl font-bold mb-4 text-center text-gray-800">Buy Opus Land 🌴</h2>
           <nav className="text-center text-gray-500 mb-4">
-            <p className="cursor-pointer" onClick={() => handleCopyToClipboard(publicKey)}>
-              Public Key: {publicKey}
-              {copySuccess === publicKey && <span className="text-green-500 text-sm ml-2">Copied to clipboard!</span>}
-            </p>
-            <p className="cursor-pointer" onClick={() => handleCopyToClipboard(privateKey)}>
-              Private Key: 🤫 {privateKey}
-              {copySuccess === privateKey && <span className="text-green-500 text-sm ml-2">Copied to clipboard!</span>}
-            </p>
+           
           </nav>
-          <div className="w-full max-w-screen-lg">
+          <div className="w-full max-w-screen-lg flex-justify-center">
             <OpusMap publicKey={publicKey} privateKey={privateKey} />
           </div>
           <div className="mt-8">
